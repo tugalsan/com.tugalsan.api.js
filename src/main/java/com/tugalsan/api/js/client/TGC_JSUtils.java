59b67547e172f17cc98eb36944515812d06dd10e
tugalsan karabacak <tugalsan@gmail.com>
2022-06-20 23:06:23 +0300

    Initial commit

---
+++ src/main/java/com/tugalsan/api/js/client/TGC_JSUtils.java
@@ -0,0 +1,53 @@
+package com.tugalsan.api.js.client;
+
+import com.google.gwt.dom.client.Element;/*DONT TOUCH ME*/
+import com.google.gwt.core.client.ScriptInjector;
+import com.google.gwt.dom.client.*;
+import com.google.gwt.user.client.ui.Widget;
+ 
+public class TGC_JSUtils {
+
+    public static native NodeList<Element> querySelectorAll(String tag_dot_className) /*-{
+        return $doc.querySelectorAll(tag_dot_className)
+ }-*/;
+
+    public static void injectJSFromURL(CharSequence url) {
+        ScriptInjector.fromUrl(url.toString()).setRemoveTag(false).setWindow(ScriptInjector.TOP_WINDOW).inject();
+    }
+
+    public static void injectJSFromString(CharSequence text) {
+        ScriptInjector.fromString(text.toString()).setRemoveTag(false).setWindow(ScriptInjector.TOP_WINDOW).inject();
+    }
+
+    public static native void evalJSCode(Element parent, CharSequence jsCode);
+
+    public static void evalJSCode(Widget parent, CharSequence jsCode) {
+        evalJSCode(parent.getElement(), jsCode);
+    }
+
+    public static native String includeJS(CharSequence filename) /*-{
+        return $wnd.includeJS(filename);
+}-*/;
+
+    public static native boolean isBrowserUpToDate() /*-{
+        return $wnd.isBrowserUpToDate();
+}-*/;
+
+    public static native String includeJM(CharSequence filename) /*-{
+        return $wnd.includeJM(filename);
+}-*/;
+
+    public static native String onMyButtonClick() /*-{//EXAMPLE
+        return $wnd.myJSfunction();
+}-*/;
+
+    public static native String addJSElement(CharSequence parentId, CharSequence childId) /*-{
+        console.log('parentId:' + parentId);
+        console.log('childId:' + childId);
+        var p = document.getElementById(parentId);
+        console.log('p:' + p);
+        var c = document.getElementById(childId);
+        console.log('c:' + c);
+        document.getElementById(parentId).appendChild(document.getElementById(childId));
+}-*/;
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module com.tugalsan.api.js {
+    requires gwt.user;
+    exports com.tugalsan.api.js.client;
+}
